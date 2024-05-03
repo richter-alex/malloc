f07b370697ce5e487eb2ed52a5eb5040d649dd41
Alex Richter <anprichter@gmail.com>
2024-05-02 21:53:31 -0600

    Some progress on free()
    Still broken, though.

--- main.c
+++ main.c
@@ -30,12 +30,12 @@ struct Free_List * free_list_ptr = &free_list;
 
 void init_allocator() {
     free_list_ptr->heap = mmap(NULL, HEAP_SIZE, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANON, -1, 0);
-        
+
     if (free_list_ptr->heap == MAP_FAILED) {
         printf("mmap failed. errno: %d\n", errno);
         exit(-1);
     }
-    
+
     free_list_ptr->heap_used = 0;
     free_list_ptr->first_free = (struct Free_List_Node *)free_list_ptr->heap;
     free_list_ptr->first_free->size = HEAP_SIZE - sizeof(struct Free_List_Node);
@@ -69,6 +69,8 @@ void * mr_alloc(size_t size) {
     return ptr;
 }
 
+// Need to approach this one with a fresh mind. Consecutive free() calls and not leaving the linked list in
+// the correct state.
 void mr_free(void * ptr) {
         struct Allocation_Header * alloc_ptr = (struct Allocation_Header*)ptr - 1;
         size_t size = alloc_ptr->size;
@@ -79,9 +81,8 @@ void mr_free(void * ptr) {
         fl_node.size = (size + sizeof(struct Allocation_Header)) - sizeof(struct Free_List_Node);
         fl_node.next = free_list_ptr->first_free;
         *(struct Free_List_Node *)alloc_ptr = fl_node;
-        free_list_ptr->first_free = (struct Free_List_Node *)alloc_ptr;
 
-        return;
+        free_list_ptr->first_free = (struct Free_List_Node *)alloc_ptr;
 }
 
 int main(int argc, char **argv) {
@@ -91,6 +92,10 @@ int main(int argc, char **argv) {
     *another_alloc = 1;
     unsigned int * yet_another_alloc  = (unsigned int *)mr_alloc(sizeof(unsigned int) * 5);
     *yet_another_alloc = 128;
+
+    mr_free(new_alloc);
+    mr_free(another_alloc);
+    mr_free(yet_another_alloc);
 
     return 0;
 }
